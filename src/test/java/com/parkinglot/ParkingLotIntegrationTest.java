e4dc53741498b50a5ebcc15b30eb5557f4fa4bdd
RAJAT GUPTA <rajat.hbtu@gmail.com>
2019-02-17 18:39:19 +0530

    test_cases_minor_changes

--- src/test/java/com/parkinglot/ParkingLotIntegrationTest.java
+++ src/test/java/com/parkinglot/ParkingLotIntegrationTest.java
@@ -13,7 +13,6 @@ import java.util.Map;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-
 public class ParkingLotIntegrationTest {
 
     static IParkingLot pkLot;
